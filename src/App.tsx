66ad8449c3523c38b69ea1a8aff7e0f42ada1453
priyam2701 <priyam.bhat@ecosmob.com>
2024-03-19 12:30:52 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+
+
+const App = () => {
+  return (
+   <Router>
+    {/* HEADER  */}
+<Routes>
+  <Route />
+</Routes>
+   </Router>
+  )
+}
+
+export default App
